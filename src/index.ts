65109aeedf6a5230ed325479dc237781cc53451d
airtoxin <airtoxin@icloud.com>
2020-04-29 01:13:31 +0900

    Use either-async-only

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Codec, Either, EitherAsync, GetInterface, Left, number, Right, string } from "purify-ts";
+import { Codec, EitherAsync, GetInterface, Left, number, Right, string } from "purify-ts";
 import ExtensibleCustomError from "extensible-custom-error";
 
 type UserCreationRequestParameter = GetInterface<typeof UserCreationRequestParameter>;
@@ -17,20 +17,20 @@ type UserResponse = User;
 
 class ValidationError extends ExtensibleCustomError {}
 
-const validate = (requestParams: unknown): Either<ValidationError, UserCreationRequestParameter> =>
-  UserCreationRequestParameter.decode(requestParams)
+const validate = (requestParams: unknown): EitherAsync<ValidationError, UserCreationRequestParameter> => EitherAsync(async ({ liftEither }) =>
+  liftEither(UserCreationRequestParameter.decode(requestParams)
     .mapLeft(reason => new ValidationError(reason))
     .chain(rp => {
       if (rp.age < 0) return Left(new ValidationError(`age must be bigger than 0. got=${rp.age}`));
       if (rp.name === "") return Left(new ValidationError(`name must be non empty.`));
       if (rp.name.length > 30) return Left(new ValidationError(`name length must be shorter than 30. got=${rp.name}`));
       return Right(rp);
-    });
+    })));
 
 class UserCreationError extends ExtensibleCustomError {}
 
-const insertUser = async (userCreationRequestParameter: UserCreationRequestParameter): Promise<Either<UserCreationError, User>> =>
-  Math.random() > 0.5 ? Right({ id: (Math.random() * 10e8).toFixed(), ...userCreationRequestParameter }) : Left(new UserCreationError(`User insertion error`));
+const insertUser = (userCreationRequestParameter: UserCreationRequestParameter): EitherAsync<UserCreationError, User> => EitherAsync(async ({ fromPromise }) =>
+  fromPromise(Promise.resolve(Math.random() > 0.5 ? Right({ id: (Math.random() * 10e8).toFixed(), ...userCreationRequestParameter }) : Left(new UserCreationError(`User insertion error`)))));
 
 type Request = {
   params: unknown;
@@ -47,9 +47,9 @@ const handleError = (res: Response) => (error: Error) => {
   return res.sendError(500, "Internal server error");
 }
 export const handleRequest = (req: Request, res: Response): void => {
-  EitherAsync<ValidationError | UserCreationError, UserResponse>(async ({ liftEither, fromPromise }) => {
-    const requestParameter = await liftEither(validate(req.params));
-    return fromPromise(insertUser(requestParameter));
+  EitherAsync<ValidationError | UserCreationError, UserResponse>(async ({ fromPromise }) => {
+    const requestParameter = await fromPromise(validate(req.params).run());
+    return fromPromise(insertUser(requestParameter).run());
   }).run().then(result => result.either(handleError(res), user => res.sendBody(user)));
 };
 
